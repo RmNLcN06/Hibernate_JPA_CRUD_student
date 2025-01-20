1cb68db15576c75596bc8b5b487520c37462a9c2
RmNLcN06 <romain.luciano@orange.fr>
2025-01-20 16:17:50 +0100

    Add Student entity with annotations and fields

---
+++ src/main/java/com/rmnlcn/Hibernate_JPA_CRUD_student/entities/Student.java
@@ -0,0 +1,30 @@
+package com.rmnlcn.Hibernate_JPA_CRUD_student.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="student")
+public class Student {
+
+    // define fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+
+    // define constructors
+
+    // define getters / setters
+
+    // define toString() method
+}
