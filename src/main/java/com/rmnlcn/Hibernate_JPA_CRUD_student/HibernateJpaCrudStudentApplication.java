2ffbea3b1b08457707fa8fd4dd16a3682765a5e3
RmNLcN06 <romain.luciano@orange.fr>
2025-01-20 14:58:30 +0100

    Add Command Line Runner

--- src/main/java/com/rmnlcn/Hibernate_JPA_CRUD_student/HibernateJpaCrudStudentApplication.java
+++ src/main/java/com/rmnlcn/Hibernate_JPA_CRUD_student/HibernateJpaCrudStudentApplication.java
@@ -1,7 +1,9 @@
 package com.rmnlcn.Hibernate_JPA_CRUD_student;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HibernateJpaCrudStudentApplication {
@@ -10,4 +12,10 @@ public class HibernateJpaCrudStudentApplication {
 		SpringApplication.run(HibernateJpaCrudStudentApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello World");
+		};
+	}
 }
