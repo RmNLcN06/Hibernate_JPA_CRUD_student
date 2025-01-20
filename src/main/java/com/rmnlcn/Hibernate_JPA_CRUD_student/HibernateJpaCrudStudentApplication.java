565bdc3b3f67c7bfdba0b4105ec57998243a0bcd
RmNLcN06 <romain.luciano@orange.fr>
2025-01-20 14:52:58 +0100

    Initial commit

---
+++ src/main/java/com/rmnlcn/Hibernate_JPA_CRUD_student/HibernateJpaCrudStudentApplication.java
@@ -0,0 +1,13 @@
+package com.rmnlcn.Hibernate_JPA_CRUD_student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateJpaCrudStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateJpaCrudStudentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rmnlcn/Hibernate_JPA_CRUD_student/HibernateJpaCrudStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmnlcn.Hibernate_JPA_CRUD_student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateJpaCrudStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
