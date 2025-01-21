6a0c95c2db23465da1f720bc5a4a6dc7c36fabf5
RmNLcN06 <romain.luciano@orange.fr>
2025-01-21 13:51:07 +0100

    Add StudentDAOImpl class & implement save method with EntityManager

---
+++ src/main/java/com/rmnlcn/Hibernate_JPA_CRUD_student/daos/StudentDAOImpl.java
@@ -0,0 +1,27 @@
+package com.rmnlcn.Hibernate_JPA_CRUD_student.daos;
+
+import com.rmnlcn.Hibernate_JPA_CRUD_student.entities.Student;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO {
+
+    // Define field for entity manager
+    private EntityManager entityManager;
+
+    // Inject entity manager using constructor injection
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    // Implement save method
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+        entityManager.persist(theStudent);
+    }
+}
