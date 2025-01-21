c8e9adbf85dae5265212bfc37ec0e82a599a7811
RmNLcN06 <romain.luciano@orange.fr>
2025-01-21 13:40:34 +0100

    Add DAO package & StudentDAO interface with save method

---
+++ src/main/java/com/rmnlcn/Hibernate_JPA_CRUD_student/daos/StudentDAO.java
@@ -0,0 +1,7 @@
+package com.rmnlcn.Hibernate_JPA_CRUD_student.daos;
+
+import com.rmnlcn.Hibernate_JPA_CRUD_student.entities.Student;
+
+public interface StudentDAO {
+    void save(Student theStudent);
+}
