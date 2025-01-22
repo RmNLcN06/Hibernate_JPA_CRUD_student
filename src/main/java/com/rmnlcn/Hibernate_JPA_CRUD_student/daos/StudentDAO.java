90939f09c556ac2343e40190c5d6910d3322f6ec
RmNLcN06 <romain.luciano@orange.fr>
2025-01-22 14:13:43 +0100

    Add findAll method to DAO interface

--- src/main/java/com/rmnlcn/Hibernate_JPA_CRUD_student/daos/StudentDAO.java
+++ src/main/java/com/rmnlcn/Hibernate_JPA_CRUD_student/daos/StudentDAO.java
@@ -2,9 +2,13 @@ package com.rmnlcn.Hibernate_JPA_CRUD_student.daos;
 
 import com.rmnlcn.Hibernate_JPA_CRUD_student.entities.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
     void save(Student theStudent);
 
     Student findById(Integer id);
+
+    List<Student> findAll();
 }
