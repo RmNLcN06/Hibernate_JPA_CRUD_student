f0d3a33507938f1c3fbe124a02de6c058b432ef6
RmNLcN06 <romain.luciano@orange.fr>
2025-01-23 10:40:31 +0100

    Update main app with update method

--- src/main/java/com/rmnlcn/Hibernate_JPA_CRUD_student/HibernateJpaCrudStudentApplication.java
+++ src/main/java/com/rmnlcn/Hibernate_JPA_CRUD_student/HibernateJpaCrudStudentApplication.java
@@ -23,8 +23,27 @@ public class HibernateJpaCrudStudentApplication {
 			// createMultipleStudents(studentDAO);
 			// readStudent(studentDAO);
 			// queryForStudents(studentDAO);
-			queryForStudentsByLastName(studentDAO);
+			// queryForStudentsByLastName(studentDAO);
+			updateStudent(studentDAO);
 		};
+	}
+
+	private void updateStudent(StudentDAO studentDAO) {
+
+		// retrieve student based on the id: primary key
+		int studentId = 1;
+		System.out.println("Getting student with id: " + studentId);
+		Student myStudent = studentDAO.findById(studentId);
+
+		// change first name to "Paul" then back to "Romain"
+		System.out.println("Updating student ...");
+		myStudent.setFirstName("Romain");
+
+		// update the student
+		studentDAO.update(myStudent);
+
+		// display the updated student
+		System.out.println("Updated student: " + myStudent);
 	}
 
 	private void queryForStudentsByLastName(StudentDAO studentDAO) {
